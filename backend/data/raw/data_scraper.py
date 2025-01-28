8e8f409e64a1efdb4ac887f4a8a113d085e04b00
Hinali Patel <neelmahyavanshi9@gmail.com>
2025-01-27 21:13:13 -0500

    scraped the individual links of pages

--- backend/data/raw/data_scraper.py
+++ backend/data/raw/data_scraper.py
@@ -3,5 +3,9 @@
 # Rent, 
 # Bath, Bed, Sqaure ft, Street number, Street name, postal code, neighboorhood, property type, description(furnished?, parking?, pool?, heating included?, electricity included?, washer and dryer included?), 
 
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
